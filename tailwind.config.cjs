0820cdb0a37788eeb9a4722a12ef3f9fd93e11a5
Mio <mioa0012@stud.kea.dk>
2023-02-20 11:15:09 +0100

    test

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {     
+      black: {        
+        light: '#272727',        
+        DEFAULT: '#212121',        
+        dark: '#151515',     
+  },      
+       red: {        
+        light: '#CAC2C1',        
+        DEFAULT: '#C84540',        
+        dark: '#623533',      
+  },
     extend: {},
   },
+},
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
